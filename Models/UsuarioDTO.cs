ff6d1fd10b8ff4abf72b35993969d79090a4ef18
Bruno Sajermann <contato@sajermann.com>
2021-03-09 13:41:21 -0300

    Initial commit

---
+++ Models/UsuarioDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasApiEliasRibeiro.Models
+{
+    public class UsuarioDTO
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Email é Obrigatório")]
+        public string Email { get; set; }
+        [Required]
+        public string Senha { get; set; }
+        [Required]
+        [Compare("Senha")]
+        public string ConfirmacaoSenha { get; set; }
+    }
+}
