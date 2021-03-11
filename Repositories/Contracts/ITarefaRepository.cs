5561b1a8d02969daea6c4de7bea8d188fcf65f05
Bruno Sajermann <contato@sajermann.com>
2021-03-11 15:45:01 -0300

    Video 7 Complete

---
+++ Repositories/Contracts/ITarefaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinhasTarefasApiEliasRibeiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasApiEliasRibeiro.Repositories.Contracts
+{
+    public interface ITarefaRepository 
+    {
+        List<Tarefa> Sincronizacao(List<Tarefa> tarefas);
+        List<Tarefa> Restauracao(ApplicationUser usuario, DateTime? dataUltimaSincronizacao);
+    }
+}
---
+++ Repositories/TarefaRepository.cs
@@ -0,0 +1,55 @@
+﻿using MinhasTarefasApiEliasRibeiro.Database;
+using MinhasTarefasApiEliasRibeiro.Models;
+using MinhasTarefasApiEliasRibeiro.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasApiEliasRibeiro.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly MinhasTarefasContext _banco;
+
+        public TarefaRepository(MinhasTarefasContext banco)
+        {
+            _banco = banco;
+        }
+
+        public List<Tarefa> Restauracao(ApplicationUser usuario, DateTime? dataUltimaSincronizacao)
+        {
+            var query = _banco.Tarefas.Where(a=>a.UsuarioId == usuario.Id).AsQueryable();
+            
+            if(dataUltimaSincronizacao != null)
+            {
+                query.Where(a => a.Criado >= dataUltimaSincronizacao || a.Atualizado >= dataUltimaSincronizacao);
+            }
+            return query.ToList<Tarefa>();
+        }
+
+        public List<Tarefa> Sincronizacao(List<Tarefa> tarefas)
+        {
+            var tarefasNovas = tarefas.Where(a => a.IdTarefaApi == 0);
+            if(tarefasNovas.Count() > 0)
+            {
+                foreach(var tarefa in tarefasNovas)
+                {
+                    _banco.Tarefas.Add(tarefa);
+                }
+            }
+
+            var tarefasExcluidasAtualizadas = tarefas.Where(a => a.IdTarefaApi == 0);
+            if (tarefasExcluidasAtualizadas.Count() > 0)
+            {
+                foreach (var tarefa in tarefasExcluidasAtualizadas)
+                {
+                    _banco.Tarefas.Update(tarefa);
+                }
+            }
+            _banco.SaveChanges();
+
+            return tarefasNovas.ToList();
+        }
+    }
+}
